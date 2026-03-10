232dfaf8967ba3ead30742673f3368096a954a8a
Raphael Oluwapelumi john <charlesraphael63-crypto@example.com>
2026-03-10 10:31:03 +0100

    I added the Button and create a component for it

--- src/Components/dataJson.jsx
+++ src/Components/dataJson.jsx
@@ -1,12 +1,12 @@
-// export const navBar = [
-//     {id: 1, name: "Why Paystack"},
-//     {id: 2, name: "Customer"},
-//     {id: 3, name: "Pricing"},
-//     {id: 1, name: "Learn"},
-// ];
+export const navBar = [
+    {id: 1, name: "Why Paystack"},
+    {id: 2, name: "Customer"},
+    {id: 3, name: "Pricing"},
+    {id: 1, name: "Learn"},
+];
 
-// export const navBar_2 = [
-//     {id: 2, name: "Develpoers"},
-//     {id: 2, name: "Support"},
-//     {id: 3, name: "Login"}
-// ]
+export const navBar_2 = [
+    {id: 2, name: "Develpoers"},
+    {id: 2, name: "Support"},
+    {id: 3, name: "Login"}
+]
